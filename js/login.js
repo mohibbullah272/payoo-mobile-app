2fdb6f9ba5eec5e810a68c15d569596ec42c1b6c
mohibbullah272 <bullahmohi50@gmail.com>
2024-09-18 22:15:29 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// login from
+document.getElementById('btn-login').addEventListener
+('click',function(event){
+event.preventDefault();
+const phoneNumber =document.getElementById('phone-number').value;
+const pinNumber = document.getElementById('pin-number').value;
+if(phoneNumber === '019' && pinNumber === '1234'){
+    window.location.href='\home.html'
+}
+
+
+
+})
+
